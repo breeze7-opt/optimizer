531f1e3b39f985392bf349708fc85f4aecb827ec
breeze7-opt <2271380462@qq.com>
2024-08-03 12:30:17 +0800

    Add files via upload

---
+++ main_tinyimagenet.py
@@ -0,0 +1,261 @@
+
+
+import os
+import numpy as np
+import pandas as pd
+from PIL import Image
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import copy
+from sklearn.preprocessing import LabelEncoder
+import time
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, models
+from new_5 import new_5
+from AdaBound import AdaBound
+from yogi import Yogi
+from adamod import AdaMod
+from Adan import Adan
+import warnings
+warnings.filterwarnings('ignore')
+
+if torch.cuda.is_available():
+    print('CUDA is available. Working on GPU')
+    DEVICE = torch.device('cuda')
+else:
+    print('CUDA is not available. Working on CPU')
+    DEVICE = torch.device('cpu')
+DIR_MAIN = 'F/Tiny_ImageNet/tiny-imagenet-200/'
+DIR_TRAIN = DIR_MAIN + 'train/'
+DIR_VAL = DIR_MAIN + 'val/'
+DIR_TEST = DIR_MAIN + 'test/'
+
+# Number of labels - 200
+labels = os.listdir(DIR_TRAIN)
+
+# Initialize labels encoder
+encoder_labels = LabelEncoder()
+encoder_labels.fit(labels)
+
+# Create lists of files and labels for training (100'000 items)
+files_train = []
+labels_train = []
+for label in labels:
+    for filename in os.listdir(DIR_TRAIN + label + '/images/'):
+        files_train.append(DIR_TRAIN + label + '/images/' + filename)
+        labels_train.append(label)
+
+# Create lists of files and labels for validation (10'000 items)
+files_val = []
+labels_val = []
+for filename in os.listdir(DIR_VAL + 'images/'):
+    files_val.append(DIR_VAL + 'images/' + filename)
+
+val_df = pd.read_csv(DIR_VAL + 'val_annotations.txt', sep='\t', names=["File", "Label", "X1", "Y1", "X2", "Y2"],
+                     usecols=["File", "Label"])
+for f in files_val:
+    l = val_df.loc[val_df['File'] == f[len(DIR_VAL + 'images/'):]]['Label'].values[0]
+    labels_val.append(l)
+
+# List of files for testing (10'000 items)
+files_test = []
+for filename in os.listdir(DIR_TEST + 'images/'):
+    files_test.append(DIR_TEST + 'images/' + filename)
+    files_test = sorted(files_test)
+'''
+print("The first five files from the list of train images:", files_train[:5])
+print("\nThe first five labels from the list of train labels:", labels_train[:5])
+print("\nThe first five files from the list of validation images:", files_val[:5])
+print("\nThe first five labels from the list of validation labels:", labels_val[:5])
+print("\nThe first five files from the list of test images:", files_test[:5])
+'''
+
+class ImagesDataset(Dataset):
+    def __init__(self, files, labels, encoder, transforms, mode):
+        super().__init__()
+        self.files = files
+        self.labels = labels
+        self.encoder = encoder
+        self.transforms = transforms
+        self.mode = mode
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        pic = Image.open(self.files[index]).convert('RGB')
+
+        if self.mode == 'train' or self.mode == 'val':
+            x = self.transforms(pic)
+            label = self.labels[index]
+            y = self.encoder.transform([label])[0]
+            return x, y
+        elif self.mode == 'test':
+            x = self.transforms(pic)
+            return x, self.files[index]
+
+
+transforms_train = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262]),
+    transforms.RandomErasing(p=0.5, scale=(0.06, 0.08), ratio=(1, 3), value=0, inplace=True)
+])
+
+transforms_val = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.4802, 0.4481, 0.3975], [0.2302, 0.2265, 0.2262])
+])
+
+train_dataset = ImagesDataset(files=files_train,
+                              labels=labels_train,
+                              encoder=encoder_labels,
+                              transforms=transforms_train,
+                              mode='train')
+
+val_dataset = ImagesDataset(files=files_val,
+                            labels=labels_val,
+                            encoder=encoder_labels,
+                            transforms=transforms_val,
+                            mode='val')
+
+test_dataset = ImagesDataset(files=files_test,
+                             labels=None,
+                             encoder=None,
+                             transforms=transforms_val,
+                             mode='test')
+
+
+def training(model, model_name, num_epochs, train_dataloader, val_dataloader):
+    loss_function = torch.nn.CrossEntropyLoss()
+    #optimizer = torch.optim.SGD(model.parameters(), lr=0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    #optimizer = AdaBound(model.parameters())
+    print(optimizer)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.33)
+
+    train_loss_array = []
+    train_acc_array = []
+    val_loss_array = []
+    val_acc_array = []
+    time_train = []
+    time_test = []
+    lowest_val_loss = np.inf
+    best_model = None
+    target_accuracy = 0.57 #87
+    start = time.time()
+    stop_training = False
+    for epoch in tqdm(range(num_epochs)):
+
+        print('Epoch: {} | Learning rate: {}'.format(epoch + 1, scheduler.get_lr()))
+
+        for phase in ['train', 'val']:
+
+            epoch_loss = 0
+            epoch_correct_items = 0
+            epoch_items = 0
+
+            if phase == 'train':
+                model.train()
+                train_time_1 = time.time()
+                with torch.enable_grad():
+                    for samples, targets in train_dataloader:
+                        samples = samples.to(DEVICE)
+                        targets = targets.to(DEVICE)
+
+                        optimizer.zero_grad()
+                        outputs = model(samples)
+                        loss = loss_function(outputs, targets)
+                        preds = outputs.argmax(dim=1)
+                        correct_items = (preds == targets).float().sum()
+
+                        loss.backward()
+                        optimizer.step()
+
+                        epoch_loss += loss.item()
+                        epoch_correct_items += correct_items.item()
+                        epoch_items += len(targets)
+                #print(epoch_items)
+                train_time_2 = time.time()
+                time_train.append(train_time_2-train_time_1)
+                print('train Accuracy: {}/{} ({:.2f}%)'.format(epoch_correct_items,epoch_items,100.*epoch_correct_items / epoch_items))
+                print('该轮训练时间：{}s'.format(train_time_2-train_time_1))
+                train_loss_array.append(epoch_loss / epoch_items)
+                train_acc_array.append(epoch_correct_items / epoch_items)
+
+                scheduler.step()
+
+            elif phase == 'val':
+                model.eval()
+                test_time_1 = time.time()
+                with torch.no_grad():
+                    for samples, targets in val_dataloader:
+                        samples = samples.to(DEVICE)
+                        targets = targets.to(DEVICE)
+
+                        outputs = model(samples)
+                        loss = loss_function(outputs, targets)
+                        preds = outputs.argmax(dim=1)
+                        correct_items = (preds == targets).float().sum()
+
+                        epoch_loss += loss.item()
+                        epoch_correct_items += correct_items.item()
+                        epoch_items += len(targets)
+                #print(epoch_items)
+                print('test Accuracy: {}/{} ({:.2f}%)'.format(epoch_correct_items,epoch_items,100.*epoch_correct_items / epoch_items))
+                test_time_2 = time.time()
+                time_test.append(test_time_2-test_time_1)
+                print('该轮测试时间：{}s'.format(test_time_2-test_time_1))
+                val_loss_array.append(epoch_loss / epoch_items)
+                val_acc_array.append(epoch_correct_items / epoch_items)
+
+                if epoch_loss / epoch_items < lowest_val_loss:
+                    lowest_val_loss = epoch_loss / epoch_items
+                    #torch.save(model.state_dict(), '{}_weights.pth'.format(model_name))
+                    best_model = copy.deepcopy(model)
+                    print("\t| New lowest val loss for {}: {}".format(model_name, lowest_val_loss))
+                if  val_acc_array[epoch-1] >= target_accuracy:
+                    use_time = time.time()
+                    time_taken = use_time - start
+                    print("达到精度 {}% 所用的时间：{} 秒".format(target_accuracy, time_taken))
+                    #break
+                    stop_training = True
+    
+                if stop_training:
+                    break
+
+    return best_model, train_loss_array, train_acc_array, val_loss_array, val_acc_array, time_train, time_test
+train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False)
+num_epochs = 100
+model_vgg19_bn = models.vgg19_bn(pretrained=True)
+for param in model_vgg19_bn.parameters():
+    param.requires_grad = False
+model_vgg19_bn.classifier[6] = torch.nn.Linear(in_features=model_vgg19_bn.classifier[6].in_features, out_features=200)
+model_vgg19_bn = model_vgg19_bn.to(DEVICE)
+
+vgg19_bn_training_results = training(model=model_vgg19_bn,
+                                     model_name='VGG19_bn',
+                                     num_epochs=num_epochs,
+                                     train_dataloader=train_dataloader,
+                                     val_dataloader=val_dataloader)
+
+model_vgg19_bn, train_loss_array, train_acc_array, val_loss_array, val_acc_array, time_train, time_test = vgg19_bn_training_results
+
+min_loss = min(val_loss_array)
+min_loss_epoch = val_loss_array.index(min_loss)
+min_loss_accuracy = val_acc_array[min_loss_epoch]
+
+
+print("\nTraining results:")
+print("\tMin val loss {:.4f} was achieved during epoch #{}".format(min_loss, min_loss_epoch + 1))
+print("\tVal accuracy during min val loss is {:.4f}".format(min_loss_accuracy))
+max_test_acc = 100.*max(val_acc_array)
+max_train_acc= 100.*max(train_acc_array)
+print('max training acc is {:.2f}% | max val acc is {:.2f}%'.format(max_train_acc,max_test_acc))
+print('trian time:', time_train)
+print('test time:', time_test)
