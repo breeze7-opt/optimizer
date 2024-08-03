fa450ba29c51c7039d3e3ca04ea6facb31816f73
breeze7-opt <2271380462@qq.com>
2024-08-03 12:49:13 +0800

    Update cifar_time.py

--- cifar_time.py
+++ cifar_time.py
@@ -9,17 +9,14 @@ import torchvision
 import time
 from ResNet import *
 from VGG16 import *
-from adabelief import AdaBelief
 from yogi import Yogi
 from AdaBound import AdaBound
 from adamod import AdaMod
 from Adan import Adan
-from AdaMG import AdaMG
-#from aggmo import AggMo
-#from lamb import *
+
 
 batch_size = 64
-'''设置数据集'''
+'''datasets'''
 train_data = torchvision.datasets.CIFAR100(root="F/CIFAR100", train=True,
                                           transform=torchvision.transforms.ToTensor(),
                                           download=True)
@@ -33,19 +30,16 @@ test_data_size = len(test_data)
 train_loader = torch.utils.data.DataLoader(dataset=train_data,batch_size=batch_size,shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_data,batch_size=batch_size,shuffle=False)
 
-'''建立网络'''
-
-
-#定义网络
+'''model'''
 model =ResNet34().cuda()
 
-'''挑选优化器'''
-from new_5 import new_5
+'''optimizer'''
+from AdaGC import AdaGC
 #optimizer =AdaBound(model.parameters(), lr= 0.001)
 optimizer = optim.SGD(model.parameters(), lr = 0.001, momentum=0.9)
 print(optimizer)
 
-'''损失函数'''
+'''loss'''
 criterion = nn.CrossEntropyLoss()
 if torch.cuda.is_available():
     criterion = criterion.cuda()
@@ -59,7 +53,7 @@ time_train = []
 time_test = []
 target_accuracy =54 #85.5 #87
 
-'''开始训练'''
+'''train'''
 def train(epoch):
     print('           第 {} 轮'.format(epoch))
     train_time_1 = time.time()
@@ -71,14 +65,14 @@ def train(epoch):
         if torch.cuda.is_available():
             data = data.cuda()
             target = target.cuda()
-        optimizer.zero_grad()   #清空过往梯度
+        optimizer.zero_grad()
         output = model(data)
         loss = criterion(output,target)
         total_train_loss +=loss.item()
         train_acc = (output.argmax(1) == target).sum()
         total_train_acc +=  train_acc
-        loss.backward()    #反向传播
-        optimizer.step()   #根据梯度更新网络参数
+        loss.backward()
+        optimizer.step()
     train_loss.append(total_train_loss)
     train_accs.append((100.* int(total_train_acc.item()) / train_data_size))
 
@@ -88,9 +82,7 @@ def train(epoch):
     print("训练集上的Loss：{}".format(total_train_loss))
     print("训练集上的准确率：{}%".format(int(total_train_acc.item()) * 100 / train_data_size))
     print('该轮训练时间：{}s'.format(train_time_2-train_time_1))
-        #if batch_idx % 50 == 0:
-         #   print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data), len(train_loader.dataset),
-         #              100. * batch_idx / len(train_loader), loss.item()))
+
 def test():
     model.eval()
     total_test_loss = 0
@@ -115,20 +107,11 @@ def test():
     print("整体测试集上的Loss：{}".format(total_test_loss))
     print("整体测试集上的准确率：{}%".format(int(total_test_acc.item()) * 100 / test_data_size))
     print('该轮测试时间：{}s'.format(test_time_2-test_time_1))
-    '''
-    if (int(total_test_acc.item()) * 100 / test_data_size) >= target_accuracy:
-        use_time = time.time()
-        time_taken = use_time - start
-        print("达到精度 {}% 所用的时间：{} 秒".format(target_accuracy, time_taken))
-        break
-    '''
-    #print('           第 {} 轮'.format(epoch))
-    #print('Test set: Average loss: {:.4f}'.format(test_loss))
-    #print('Accuracy: {}/{} ({:.2f}%)'.format(correct, test_data_size,100. * correct / test_data_size))
+
+   
 for epoch in range(1, 201):
     train(epoch)
     test()
-    # 判断是否达到目标准确率，达到则跳出循环
     if  test_accs[epoch-1] >= target_accuracy:
         use_time = time.time()
         time_taken = use_time - start
@@ -139,7 +122,6 @@ print('total train loss',train_loss)
 print('total test loss',test_loss)
 print('total train acc',train_accs)
 print('total test acc',test_accs)
-#print('所用时间为：',end-start,'s')
 print('最大精度： {:.2f}%'.format(max(test_accs)))
 print('train time list', time_train)
 print('test time list', time_test)
